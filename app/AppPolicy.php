9b2f2200f4a1fb25ead18f5e817bceb3b3a79a35
yudantoanas <youdant@gmail.com>
2020-05-01 15:47:25 +0700

    update

--- app/AppPolicy.php
+++ app/AppPolicy.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AppPolicy extends Model
 {
+    public $incrementing = false;
     protected $table = 'app_policies';
     protected $primaryKey = 'name';
     protected $fillable = ['name', 'content'];
