d5e87d2b120e3519072f9b17aa7048f7c294230c
yudantoanas <youdant@gmail.com>
2020-05-22 14:23:53 +0700

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -46,7 +46,7 @@ class AdminController extends Controller
      */
     public function create()
     {
-        return view('admin.create', ['roles' => Role::all()]);
+        return view('admin.create', ['roles' => Role::all(), 'menu' => 'manageAdmin']);
     }
 
     /**
