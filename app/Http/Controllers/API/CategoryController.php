8b00bbffe441952579794404903ce178d8654859
yudantoanas <youdant@gmail.com>
2020-06-13 19:16:23 +0700

    update

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Category;
 use App\Channel;
 use App\Http\Controllers\Controller;
+use App\User;
 use App\Video;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
@@ -40,8 +41,7 @@ class CategoryController extends Controller
         $authID = auth('api')->id();
         $now = Carbon::now()->toDateTimeString();
 
-        $videos = Video::where('category_id', $categoryId)
-            ->withCount('views as views')
+        $videos = Video::withCount('views as views')
             ->with([
                 'channel' => function ($query) {
                     $query->select(['id', 'name', 'thumbnail']);
@@ -54,7 +54,9 @@ class CategoryController extends Controller
                 },
                 'labels'
             ])
-            ->where('published_at', '<=', $now);
+            ->whereNotIn('channel_id', User::find($authID)->blacklistChannels->pluck('id'))
+            ->where('published_at', '<=', $now)
+            ->where('category_id', $categoryId);
 
         if ($categoryId != 5) {
             $videos->inRandomOrder();
@@ -66,9 +68,7 @@ class CategoryController extends Controller
         foreach ($videos->paginate(10)->toArray()["data"] as $video) {
             $video["is_saved_later"] = Video::find($video["id"])->users->contains($video["id"]);
             $video["channel"]["is_followed"] = Channel::find($video["channel"]["id"])->followers->contains($authID);
-            if (!Channel::find($video["channel"]["id"])->blacklists->contains($authID)) {
-                array_push($videoArray, $video);
-            }
+            array_push($videoArray, $video);
         }
 
         return $this->successResponseWithData($videoArray);
