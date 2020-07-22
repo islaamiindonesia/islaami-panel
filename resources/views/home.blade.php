a703740388c9b59bbf2d752254aeddc13cea5921
yudantoanas <youdant@gmail.com>
2020-07-22 08:20:55 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,6 +45,23 @@
                             @endif
                         </div>
                     </div>
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{ $channels->count() }}</h3>
+
+                                <p>Jumlah Kanal</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-ios-logo-youtube"></i>
+                            </div>
+                            @if(!Auth::user()->hasRole('super admin'))
+                                <a href="{{ route('admin.channels.all') }}" class="small-box-footer">Lihat Selengkapnya <i
+                                        class="fas fa-arrow-circle-right"></i></a>
+                            @endif
+                        </div>
+                    </div>
                 @endif
             </div>
             <!-- /.row -->
